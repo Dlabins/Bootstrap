868a2818498c3040782914c2ff54dea5cbd5a042
Dlabins <DLABINS1895@GMAIL.COM>
2020-11-14 12:31:39 -0500

    added js file and linked buttons to github pages I was to display

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+//document.querySelector("#btn-warning")
+function visitCodequiz(){
+    window.location = "https://dlabins.github.io/Code-Quiz/index.html"
+}
+function visitProject(){
+    window.location = "https://shannonquinn91.github.io/Drury-Lane-Sweets/index.html"
+}
+function visitPassword(){
+    window.location = "https://dlabins.github.io/PasswordGenerator/"
+}
